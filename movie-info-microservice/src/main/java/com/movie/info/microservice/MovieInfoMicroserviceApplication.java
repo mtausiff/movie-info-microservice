447d58ddfb022d1717fbbcceedb01cc2787de1c9
mtausiff <tausifcse17@gmail.com>
2019-12-15 21:11:44 +0300

    Initial movie-info-microservice project commit

---
+++ movie-info-microservice/src/main/java/com/movie/info/microservice/MovieInfoMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.movie.info.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoMicroserviceApplication.class, args);
+	}
+
+}
---
+++ movie-info-microservice/src/test/java/com/movie/info/microservice/MovieInfoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.info.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
